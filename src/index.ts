5d032cdc399c89953f276a8acf61f044ee8cf9f1
Khalid Rasool <45462819+RanaKhalid123@users.noreply.github.com>
2021-06-29 20:46:48 +0500

    removed con string

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,8 @@ app.use(json())
 app.use(todoRouter)
 app.use(userRouter)
 
-mongoose.connect('mongodb://khalidrasool768:Freelancing123@cluster0-shard-00-02.hzu31.mongodb.net:27017/assignment?ssl=true&replicaSet=atlas-11mki9-shard-0&authSource=admin&retryWrites=true&w=majority', {
+// mongoose.connect('mongodb://khalidrasool768:Freelancing123@cluster0-shard-00-02.hzu31.mongodb.net:27017/assignment?ssl=true&replicaSet=atlas-11mki9-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect('mongodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
